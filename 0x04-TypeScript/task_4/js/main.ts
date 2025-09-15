a6cb9547f608452b7933876e20427952d88dc4d1
zfirstalpha <salim14sadik@gmail.com>
2025-09-15 17:59:39 +0300

    Task 3: Ambient namespaces with CRUD library

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -1,20 +1,37 @@
-/// <reference path="./crud.d.ts" />
+/// <reference path="./subjects/Teacher.ts" />
+/// <reference path="./subjects/Subject.ts" />
+/// <reference path="./subjects/Cpp.ts" />
+/// <reference path="./subjects/React.ts" />
+/// <reference path="./subjects/Java.ts" />
 
-import { RowID, RowElement } from "./interface";
-import * as CRUD from "./crud.js";
+import Subjects = Subjects;
 
-// Create row object
-const row: RowElement = {
-  firstName: "Guillaume",
-  lastName: "Salva"
+// Create Teacher object
+export const cTeacher: Subjects.Teacher = {
+  firstName: "John",
+  lastName: "Doe",
+  experienceTeachingC: 10
 };
 
-// Insert row
-const newRowID: RowID = CRUD.insertRow(row);
+// Create constants for subjects
+export const cpp = new Subjects.Cpp();
+export const java = new Subjects.Java();
+export const react = new Subjects.React();
 
-// Update row with age
-const updatedRow: RowElement = { ...row, age: 23 };
-CRUD.updateRow(newRowID, updatedRow);
+// Cpp
+console.log("C++");
+cpp.setTeacher(cTeacher);
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
 
-// Delete row
-CRUD.deleteRow(newRowID);
+// Java
+console.log("Java");
+java.setTeacher(cTeacher);
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+// React
+console.log("React");
+react.setTeacher(cTeacher);
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
