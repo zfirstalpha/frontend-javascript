40afd76f47791e15feda30beaa70d7c8eb8042f5
zfirstalpha <salim14sadik@gmail.com>
2025-09-15 16:19:19 +0300

    Task 1: Teacher interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -13,24 +13,13 @@ interface Directors extends Teacher {
   numberOfReports: number;
 }
 
-// Example teacher
-const teacher3: Teacher = {
-  firstName: "eyasu",
-  lastName: "teklie",
-  fullTimeEmployee: false,
-  location: "addis ababa",
-  contract: false,
-};
-
-console.log("Teacher:", teacher3);
-
 // Example director
 const director1: Directors = {
   firstName: "henok",
   lastName: "lam",
+  location: "aa",
   fullTimeEmployee: true,
-  location: "Addis Ababa",
-  numberOfReports: 5,
+  numberOfReports: 17,
 };
 
-console.log("Director:", director1);
+console.log(director1);
